ca92d0d16ece17bdea367f2715ff40e7bdbfb3e5
Nguen Kuok Nhat Anh <ninja_mos@mail.ru>
2024-06-04 00:23:26 +0300

    number, remember me, filter, search, bottom bar, russian, description, themes, language, translate, admin

--- app/src/main/java/com/example/kr/HomeActivity.java
+++ app/src/main/java/com/example/kr/HomeActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Locale;
 
@@ -17,6 +21,7 @@ public class HomeActivity extends BaseActivity {
 
     private FirebaseAuth mAuth;
     private SharedPreferences sharedPreferences;
+    private boolean isAdmin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +29,50 @@ public class HomeActivity extends BaseActivity {
         setContentView(R.layout.activity_home);
 
         mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            db.collection("users").document(currentUser.getUid()).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String role = document.getString("role");
+                        isAdmin = "admin".equals(role);
+                    }
+                } else {
+                    Toast.makeText(this, "Ошибка при получении пользовательских данных", Toast.LENGTH_SHORT).show();
+                }
+                setupBottomNavigation();
+            });
+        } else {
+            setupBottomNavigation();
+        }
+    }
+
+    private void setupBottomNavigation() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.getMenu().clear(); // Сначала очищаем меню
+        bottomNavigationView.inflateMenu(R.menu.bottom_navigation_menu); // Затем инфлейтим наше меню
+
+        if (isAdmin) {
+            bottomNavigationView.getMenu().add(Menu.NONE, R.id.action_admin, Menu.NONE, getString(R.string.admin))
+                    .setIcon(R.drawable.ic_admin);
+        }
+
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.action_manufacturers) {
                 openManufacturersActivity();
                 return true;
+            }  else if (itemId == R.id.action_language_theme) {
+                showLanguageThemeDialog();
+                return true;
+            } else if (itemId == R.id.action_admin && isAdmin) {
+                openAdminActivity();
+                return true;
             } else if (itemId == R.id.action_logout) {
                 logoutUser();
-                return true;
-            } else if (itemId == R.id.action_language) {
-                showLanguageDialog();
-                return true;
-            } else if (itemId == R.id.action_theme) {
-                showThemeDialog();
                 return true;
             } else {
                 Toast.makeText(HomeActivity.this, "Выбран пункт меню с id: " + itemId, Toast.LENGTH_SHORT).show();
@@ -47,13 +81,18 @@ public class HomeActivity extends BaseActivity {
         });
     }
 
+    private void openAdminActivity() {
+        Intent intent = new Intent(HomeActivity.this, AdminActivity.class);
+        startActivity(intent);
+    }
+
     private void openManufacturersActivity() {
         Intent intent = new Intent(HomeActivity.this, ManufacturersActivity.class);
         startActivity(intent);
     }
 
     private void logoutUser() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.AlertTheme);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertTheme);
         builder.setMessage(getString(R.string.logout))
                 .setPositiveButton(getString(R.string.yes), (dialog, which) -> {
                     mAuth.signOut();
@@ -77,7 +116,6 @@ public class HomeActivity extends BaseActivity {
         finish();
         startActivity(intent);
     }
-
 
     private void showLanguageDialog() {
         String[] languages = {getString(R.string.russian), getString(R.string.english)};
@@ -115,6 +153,23 @@ public class HomeActivity extends BaseActivity {
                             break;
                         case 3:
                             applyTheme(R.style.Theme_Brown_Dark);
+                            break;
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void showLanguageThemeDialog() {
+        String[] options = {getString(R.string.lan), getString(R.string.Theme)};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.choose_option))
+                .setItems(options, (dialog, which) -> {
+                    switch (which) {
+                        case 0:
+                            showLanguageDialog();
+                            break;
+                        case 1:
+                            showThemeDialog();
                             break;
                     }
                 });
